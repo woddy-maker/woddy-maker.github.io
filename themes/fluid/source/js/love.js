fa03c16b053fb77a48c63fa5d61411b57663ea56
woddy-maker <814500505@qq.com>
2020-06-29 08:14:29 +0800

    Site updated: 2020-06-29 08:14:29

--- themes/fluid/source/js/love.js
+++ themes/fluid/source/js/love.js
@@ -1,21 +1,28 @@
 !function (e, t, a) {
-  function n() {
-    c(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 500%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"), o(), r()
+  function n()
+  {
+    c(".heart{width: 50px;height: 50px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 500%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -25px;}.heart:before{left: -25px;}"), o(), r()
   }
 
-  function r() {
-    for (var e = 0; e < d.length; e++) d[e].alpha <= 0 ? (t.body.removeChild(d[e].el), d.splice(e, 1)) : (d[e].y--, d[e].scale += .004, d[e].alpha -= .013, d[e].el.style.cssText = "left:" + d[e].x + "px;top:" + d[e].y + "px;opacity:" + d[e].alpha + ";transform:scale(" + d[e].scale + "," + d[e].scale + ") rotate(45deg);background:" + d[e].color + ";z-index:99999");
+  function r()
+  {
+    for (var e = 0; e < d.length; e++)
+    {
+      d[e].alpha <= 0 ? (t.body.removeChild(d[e].el), d.splice(e, 1)) : (d[e].y--, d[e].scale += .004, d[e].alpha -= .013, d[e].el.style.cssText = "left:" + d[e].x + "px;top:" + d[e].y + "px;opacity:" + d[e].alpha + ";transform:scale(" + d[e].scale + "," + d[e].scale + ") rotate(45deg);background:" + d[e].color + ";z-index:99999");
+    }
     requestAnimationFrame(r)
   }
 
-  function o() {
+  function o()
+  {
     var t = "function" == typeof e.onclick && e.onclick;
     e.onclick = function (e) {
       t && t(), i(e)
     }
   }
 
-  function i(e) {
+  function i(e)
+  {
     var a = t.createElement("div");
     a.className = "heart", d.push({
       el: a,
@@ -27,18 +34,22 @@
     }), t.body.appendChild(a)
   }
 
-  function c(e) {
+  function c(e)
+  {
     var a = t.createElement("style");
     a.type = "text/css";
-    try {
+    try
+    {
       a.appendChild(t.createTextNode(e))
-    } catch (t) {
+    } catch (t)
+    {
       a.styleSheet.cssText = e
     }
     t.getElementsByTagName("head")[0].appendChild(a)
   }
 
-  function s() {
+  function s()
+  {
     return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
   }
 
